e9714c18eb8d5cefcbad5aed2abaf588c8376d6c
yashbudhia <yeshbudhia@gmail.com>
2024-04-09 22:53:40 +0530

    checkpoint

---
+++ backend-complete/app.js
@@ -0,0 +1,157 @@
+const express = require("express");
+const path = require("path");
+const mongoose = require("mongoose");
+const session = require("express-session");
+const flash = require("connect-flash");
+const ExpressError = require("./utils/ExpressError");
+const methodOverride = require("method-override");
+const passport = require(`passport`);
+const LocalStrategy = require(`passport-local`);
+const User = require(`./models/user`);
+const mongoSanitize = require("express-mongo-sanitize");
+const helmet = require(`helmet`);
+const MongoStore = require("connect-mongo");
+const bodyParser = require("body-parser");
+// const dbUrl = process.env.DB_URL;
+const dbUrl =
+  "mongodb+srv://landfillapp:maihumaderchud@cluster0.wracgg2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+const userRoutes = require(`./routes/users`);
+const landRoutes = require("./routes/lands");
+const reviewRoutes = require("./routes/reviews");
+
+mongoose.connect(dbUrl, {});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+  console.log("Database connected");
+});
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.set("views", path.join(__dirname, "views"));
+app.use(
+  mongoSanitize({
+    replaceWith: "_",
+  })
+);
+app.use(helmet({}));
+
+const scriptSrcUrls = [
+  "https://stackpath.bootstrapcdn.com/",
+  `https://getbootstrap.com/`,
+  "https://api.tiles.mapbox.com/",
+  "https://api.mapbox.com/",
+  "https://kit.fontawesome.com/",
+  "https://cdnjs.cloudflare.com/",
+  "https://cdn.jsdelivr.net",
+  "https://cdn.jsdelivr.net",
+  "https://cdn.jsdelivr.net/",
+];
+const styleSrcUrls = [
+  "https://stackpath.bootstrapcdn.com/",
+  "https://kit-free.fontawesome.com/",
+  "https://stackpath.bootstrapcdn.com/",
+  "https://api.mapbox.com/",
+  "https://api.tiles.mapbox.com/",
+  "https://fonts.googleapis.com/",
+  "https://use.fontawesome.com/",
+  "https://cdn.jsdelivr.net/",
+];
+const connectSrcUrls = [
+  "https://api.mapbox.com/",
+  "https://a.tiles.mapbox.com/",
+  "https://b.tiles.mapbox.com/",
+  "https://events.mapbox.com/",
+];
+const fontSrcUrls = [];
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      defaultSrc: [],
+      connectSrc: ["'self'", ...connectSrcUrls],
+      scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
+      styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+      workerSrc: ["'self'", "blob:"],
+      objectSrc: [],
+      imgSrc: [
+        "'self'",
+        "blob:",
+        "data:",
+        "https://res.cloudinary.com/dtemmbo4i/",
+        "https://images.unsplash.com/",
+      ],
+      fontSrc: ["'self'", ...fontSrcUrls],
+    },
+  })
+);
+
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+app.use(express.static(path.join(__dirname, "public")));
+
+const store = MongoStore.create({
+  mongoUrl: dbUrl,
+  touchAfter: 24 * 60 * 60,
+  crypto: {
+    secret: "thisshouldbeabettersecret!",
+  },
+});
+
+store.on("error", function (e) {
+  console.log("SESSION STORE ERROR", e);
+});
+
+const sessionConfig = {
+  store,
+  name: "session",
+  secret: "thisshouldbeabettersecret!",
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    httpOnly: true,
+    expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+    maxAge: 1000 * 60 * 60 * 24 * 7,
+  },
+};
+app.use(session(sessionConfig));
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use((req, res, next) => {
+  res.locals.currentUser = req.user;
+  res.locals.success = req.flash("success");
+  res.locals.error = req.flash("error");
+  next();
+});
+
+app.use(`/`, userRoutes);
+app.use("/lands", landRoutes);
+app.use("/lands/:id/reviews", reviewRoutes);
+
+app.all("*", (req, res, next) => {
+  next(new ExpressError("Page Not Found", 404));
+});
+
+// app.use((err, req, res, next) => {
+//     const { statusCode = 500 } = err;
+//     if (!err.message) err.message = 'Oh No, Something Went Wrong!'
+//     res.status(statusCode).render('error', { err })
+// })
+
+app.use((err, req, res, next) => {
+  res.json({ error: err });
+});
+
+app.listen(8080, () => {
+  console.log("Serving on port 8080");
+});
+
+//
---
+++ backend-complete/public/javascripts/showPageMap.js
@@ -0,0 +1,62 @@
+
+// mapboxgl.accessToken = mapToken;
+// const map = new mapboxgl.Map({
+//     container: 'map', // container ID
+//     style: 'mapbox://styles/mapbox/streets-v12', // style URL
+//     center: campground.geometry.coordinates, // starting position [lng, lat]
+//     zoom: 9, // starting zoom
+// });
+
+// new mapboxgl.Marker
+//     .setLngLat(campground.geometry.coordinates)
+//     .addTo(map)
+
+
+// mapboxgl.accessToken = 'pk.eyJ1IjoidmFpYmhhdnJhaW5hIiwiYSI6ImNscWR1bXAxODBnNDMya21la3Fxam14bTMifQ.PDdQLPJDHDOeis3xWHlUTA';
+// const map = new mapboxgl.Map({
+// 	container: 'map', // container ID
+// 	style: 'mapbox://styles/mapbox/streets-v12', // style URL
+// 	center: [-74.5, 40], // starting position [lng, lat]
+// 	zoom: 9, // starting zoom
+// });
+
+mapboxgl.accessToken = mapToken;
+const map = new mapboxgl.Map({
+	container: 'cluster-map',
+	style: 'mapbox://styles/mapbox/streets-v12',  // stylesheet location
+	center: land.geometry.coordinates, // starting position [lng, lat]
+	zoom: 10 // starting zoom
+});
+map.addControl(new mapboxgl.NavigationControl());
+
+new mapboxgl.Marker()
+	.setLngLat(land.geometry.coordinates)
+	.setPopup(
+		new mapboxgl.Popup({ offset: 25 })
+			.setHTML(
+				`<h3>${land.title}</h3><p>${land.location}</p>`
+			)
+	)
+	.addTo(map)
+
+
+
+
+
+// mapboxgl.accessToken = mapToken;
+// const map = new mapboxgl.Map({
+// 	container: 'map',
+// 	style: 'mapbox://styles/mapbox/streets-v12',  // stylesheet location
+// 	center: land.geometry.coordinates, // starting position [lng, lat]
+// 	zoom: 10 // starting zoom
+// });
+
+// new mapboxgl.Marker()
+// 	.setLngLat(land.geometry.coordinates)
+// 	.setPopup(
+// 		new mapboxgl.Popup({ offset: 25 })
+// 			.setHTML(
+// 				`<h3>${land.title}</h3><p>${land.location}</p>`
+// 			)
+// 	)
+// 	.addTo(map)
