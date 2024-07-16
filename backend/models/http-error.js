298c02524920ebdb1372f27ac55740f37b200db8
VaibhavRaina <122040517+VaibhavRaina@users.noreply.github.com>
2024-07-16 13:34:00 +0530

    deletd

--- backend/models/http-error.js
+++
@@ -1,7 +0,0 @@
-class httpError extends Error {
-    constructor(message, errorCode) {
-        super(message);
-        this.code = errorCode;
-    }
-}
-
--- backend/routes/landfill.js
+++
@@ -1,5 +0,0 @@
-const express = require(`express`);
-const router = express.Router();
-const landfill = require(`../controllers/landfill`);
-router.get(`/`, landfill.home);
-
